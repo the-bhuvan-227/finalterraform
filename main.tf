0ff71522c064133d94ccc3e3f1c41f41176cdcea
the-bhuvan-227 <sharkwave227@gmail.com>
2022-03-21 14:07:59 +0530

    variable files also created

--- main.tf
+++ main.tf
@@ -4,3 +4,26 @@ provider "aws" {
   access_key = "AKIA6CY7YXRWMWENZ4FC"
   secret_key = "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"
 }
+
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+
+
+resource "aws_vpc" "my-custome-vpc" {
+  cidr_block       =  var.vpc_cidr_block
+ /* instance_tenancy = "default" */
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "my-custome-subnet" {
+  vpc_id     = aws_vpc.my-custome-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name = "${var.env.prefix}-subnet-1"
+  }
+}
